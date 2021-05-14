560e3d4245458b16f1ad033bc7463eb698e469f5
Unknown <wagner_gcastro@hotmail.com>
2021-05-13 22:58:10 -0300

    #01 Address: Relation belongsTo and hasMany~

--- src/database/index.js
+++ src/database/index.js
@@ -2,6 +2,8 @@ const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
 const User = require('../models/User');
+const Address = require('../models/Address');
+
 const connection = new Sequelize(dbConfig);
 
 /**
@@ -17,5 +19,9 @@ const connection = new Sequelize(dbConfig);
 //   });
 
 User.init(connection);
+Address.init(connection);
+
+User.associate(connection.models)
+Address.associate(connection.models)
 
 module.exports = connection;
