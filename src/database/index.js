002d254fe948a13663746c270ca103ecb9e29db8
Wagner Castro <wagner_gcastro@gmail.com>
2021-05-14 07:14:56 -0300

    #02 Relacionamento de muitos para muitos

--- src/database/index.js
+++ src/database/index.js
@@ -3,6 +3,7 @@ const dbConfig = require('../config/database');
 
 const User = require('../models/User');
 const Address = require('../models/Address');
+const Tech = require('../models/Tech');
 
 const connection = new Sequelize(dbConfig);
 
@@ -20,8 +21,10 @@ const connection = new Sequelize(dbConfig);
 
 User.init(connection);
 Address.init(connection);
+Tech.init(connection);
 
 User.associate(connection.models)
 Address.associate(connection.models)
+Tech.associate(connection.models);
 
 module.exports = connection;
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const UserController = require('./controllers/UserController');
 const AddressController = require('./controllers/AddressController');
+const TechController = require('./controllers/TechController');
 
 const routes = express.Router();
 
@@ -11,5 +12,11 @@ routes.post('/users', UserController.store);
 routes.get('/addresses', AddressController.index);
 routes.get('/users/:user_id/addresses', AddressController.show);
 routes.post('/users/:user_id/addresses', AddressController.store);
+
+routes.get('/techs', TechController.index);
+routes.get('/users/:user_id/techs', TechController.show);
+routes.post('/users/:user_id/techs', TechController.store);
+routes.delete('/users/:user_id/techs', TechController.delete);
+
 
 module.exports = routes;
